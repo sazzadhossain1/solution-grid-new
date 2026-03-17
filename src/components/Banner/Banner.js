1fbca2ae82d6b488e4a0264e07403636ed96b084
sazzad tomal <sazzadtomal1@gmail.com>
2026-03-18 02:06:09 +0600

    banner section done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,72 +8,90 @@ const Banner = () => {
   return (
     <div className="banner_parent_div">
       <h1>We can Help You Grow Your Business</h1>
+
       <p>
         We specialise in the Websites, Mobile Apps, Digital Marketing, Branding
         and IT Solutions
       </p>
+
       <div className="banner_grid_div">
         <Link>
-          <div className="banner_cart_one">
+          <div className="banner_cart_one banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
+            <span>Lorem ipsum dolor sit amet.</span>
+          </div>
+        </Link>
 
-            <span>Lorem ipsum dolor sit amet.</span>
-          </div>
-        </Link>
         <Link>
-          <div className="banner_cart_two">
+          <div className="banner_cart_two banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
         </Link>
+
         <Link>
-          <div className="banner_cart_three">
+          <div className="banner_cart_three banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
         </Link>
+
         <Link>
-          <div className="banner_cart_four">
+          <div className="banner_cart_four banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
         </Link>
+
         <Link>
-          <div className="banner_cart_five">
+          <div className="banner_cart_five banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
         </Link>
+
         <Link>
-          <div className="banner_cart_six">
+          <div className="banner_cart_six banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
         </Link>
+
         <Link>
-          <div className="banner_cart_saven">
+          <div className="banner_cart_saven banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
         </Link>
+
         <Link>
-          <div className="banner_cart_eight">
+          <div className="banner_cart_eight banner_card">
             <FontAwesomeIcon className="banner_icon" icon={faEnvelope} />
             <br />
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
         </Link>
       </div>
-      <Link className="banner_link_btn">
-        <div className="banner_btn_div">Hear from an expert</div>
-      </Link>
+
+      <div className="banner_btn_parent_div">
+        <Link
+          className="banner_link_btn"
+          to="https://www.vservices.com/"
+          target="_blank"
+        >
+          <div className="banner_btn_div">Hear from an expert</div>
+        </Link>
+      </div>
+      <svg width="200" height="200" viewBox="0 0 720 540">
+        <path className="st0" d="M0,28l45,477l648,24L714,0L0,28z" />
+      </svg>
     </div>
   );
 };
